93aa1782a2ac2275c4295e6b3e02840313ee32b7
Timidan <akinnusotutemitayodaniel@gmail.com>
2022-05-09 15:55:51 +0100

    add extra tests

--- contracts/LiquidityHelper.sol
+++ contracts/LiquidityHelper.sol
@@ -5,104 +5,113 @@ import "../interfaces/IUniswapV2Router01.sol";
 import "../interfaces/ILIquidityHelper.sol";
 
 contract LiquidityHelper is ILiquidityHelper {
-  error LengthMismatch();
-  IUniswapV2Router01 router;
-  address GHST;
-  address owner;
-  //0--fud
-  //1--fomo
-  //2--alpha
-  //3--kek
-  address[4] alchemicaTokens;
+    error LengthMismatch();
+    IUniswapV2Router01 router;
+    address GHST;
+    address owner;
+    //0--fud
+    //1--fomo
+    //2--alpha
+    //3--kek
+    address[4] alchemicaTokens;
 
-  constructor(
-    address[4] memory _alchemicaTokens,
-    address[] memory _pairAddresses, //might be more than 4 pairs
-    address _routerAddress,
-    address _ghst,
-    address _owner
-  ) {
-    //approve ghst
-    IERC20(_ghst).approve(_routerAddress, type(uint256).max);
-    //approve alchemica infinitely
-    for (uint256 i; i < _alchemicaTokens.length; i++) {
-      require(
-        IERC20(_alchemicaTokens[i]).approve(_routerAddress, type(uint256).max)
-      );
+    constructor(
+        address[4] memory _alchemicaTokens,
+        address[] memory _pairAddresses, //might be more than 4 pairs
+        address _routerAddress,
+        address _ghst,
+        address _owner
+    ) {
+        //approve ghst
+        IERC20(_ghst).approve(_routerAddress, type(uint256).max);
+        //approve alchemica infinitely
+        for (uint256 i; i < _alchemicaTokens.length; i++) {
+            require(
+                IERC20(_alchemicaTokens[i]).approve(
+                    _routerAddress,
+                    type(uint256).max
+                )
+            );
+        }
+        //approve pair Tokens
+        for (uint256 i; i < _pairAddresses.length; i++) {
+            require(
+                IERC20(_pairAddresses[i]).approve(
+                    _routerAddress,
+                    type(uint256).max
+                )
+            );
+        }
+
+        router = IUniswapV2Router01(_routerAddress);
+        alchemicaTokens = _alchemicaTokens;
+        GHST = _ghst;
+        owner = _owner;
     }
-    //approve pair Tokens
-    for (uint256 i; i < _pairAddresses.length; i++) {
-      require(
-        IERC20(_pairAddresses[i]).approve(_routerAddress, type(uint256).max)
-      );
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not Owner");
+        _;
     }
 
-    router = IUniswapV2Router01(_routerAddress);
-    alchemicaTokens = _alchemicaTokens;
-    GHST = _ghst;
-    owner = _owner;
-  }
-
-  modifier onlyOwner() {
-    require(msg.sender == owner, "Not Owner");
-    _;
-  }
-
-  function transferOwnership(address _newOwner) external onlyOwner {
-    owner = _newOwner;
-  }
-
-  function returnTokens(address[] calldata _tokens, uint256[] calldata _amounts)
-    external
-    onlyOwner
-  {
-    if (_tokens.length != _amounts.length) revert LengthMismatch();
-    for (uint256 i; i < _tokens.length; i++) {
-      require(IERC20(_tokens[i]).transfer(owner, _amounts[i]));
+    function transferOwnership(address _newOwner) external onlyOwner {
+        assert(_newOwner != address(0));
+        owner = _newOwner;
     }
-  }
 
-  function addLiquidity(AddLiquidityArgs calldata _args) public onlyOwner {
-    router.addLiquidity(
-      _args._tokenA,
-      _args._tokenB,
-      _args._amountADesired,
-      _args._amountBDesired,
-      _args._amountAMin,
-      _args._amountBMin,
-      address(this),
-      block.timestamp + 3000
-    );
-  }
-
-  function withdrawLiquidity(RemoveLiquidityArgs calldata _args)
-    public
-    onlyOwner
-  {
-    router.removeLiquidity(
-      _args._tokenA,
-      _args._tokenB,
-      _args._liquidity,
-      _args._amountAMin,
-      _args._amountBMin,
-      address(this),
-      block.timestamp + 3000
-    );
-  }
-
-  function batchAddLiquidity(AddLiquidityArgs[] calldata _args) external {
-    for (uint256 i; i < _args.length; i++) {
-      addLiquidity(_args[i]);
+    function returnTokens(
+        address[] calldata _tokens,
+        uint256[] calldata _amounts
+    ) external onlyOwner {
+        if (_tokens.length != _amounts.length) revert LengthMismatch();
+        for (uint256 i; i < _tokens.length; i++) {
+            require(IERC20(_tokens[i]).transfer(owner, _amounts[i]));
+        }
     }
-  }
 
-  function batchRemoveLiquidity(RemoveLiquidityArgs[] calldata _args) external {
-    for (uint256 i; i < _args.length; i++) {
-      withdrawLiquidity(_args[i]);
+    function addLiquidity(AddLiquidityArgs calldata _args) public onlyOwner {
+        router.addLiquidity(
+            _args._tokenA,
+            _args._tokenB,
+            _args._amountADesired,
+            _args._amountBDesired,
+            _args._amountAMin,
+            _args._amountBMin,
+            address(this),
+            block.timestamp + 3000
+        );
     }
-  }
 
-  function setApproval(address _token, address _spender) public onlyOwner {
-    IERC20(_token).approve(_spender, type(uint256).max);
-  }
+    function withdrawLiquidity(RemoveLiquidityArgs calldata _args)
+        public
+        onlyOwner
+    {
+        router.removeLiquidity(
+            _args._tokenA,
+            _args._tokenB,
+            _args._liquidity,
+            _args._amountAMin,
+            _args._amountBMin,
+            address(this),
+            block.timestamp + 3000
+        );
+    }
+
+    function batchAddLiquidity(AddLiquidityArgs[] calldata _args) external {
+        for (uint256 i; i < _args.length; i++) {
+            addLiquidity(_args[i]);
+        }
+    }
+
+    function batchRemoveLiquidity(RemoveLiquidityArgs[] calldata _args)
+        external
+    {
+        for (uint256 i; i < _args.length; i++) {
+            withdrawLiquidity(_args[i]);
+        }
+    }
+
+    function setApproval(address _token, address _spender) public onlyOwner {
+        IERC20(_token).approve(_spender, type(uint256).max);
+    }
 }
